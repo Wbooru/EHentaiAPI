85c9040b1d0001e7f64fe9b23158d8b95eec0029
MikiraSora <mikirasora0409@126.com>
2021-11-17 02:44:14 +0800

    add console project for test

---
+++ EHentaiAPI.TestConsole/FileSharedPreferences.cs
@@ -0,0 +1,42 @@
+﻿using EHentaiAPI.ExtendFunction;
+using EHentaiAPI.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHentaiAPI.TestConsole
+{
+    public class FileSharedPreferences : ISharedPreferences
+    {
+        private Dictionary<string, object> store = new Dictionary<string, object>();
+        private const string FILE_PATH = "config.json";
+
+        public FileSharedPreferences()
+        {
+            if (File.Exists(FILE_PATH))
+            {
+                Log.LogImplement.i("FileSharedPreferences", $"Load config from {FILE_PATH}");
+                store = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(FILE_PATH));
+            }
+        }
+
+        public T getValue<T>(string key, T defValue = default)
+        {
+            var value = store.TryGetValue(key, out var d) ? (T)d : defValue;
+            Log.LogImplement.i("FileSharedPreferences", $"Get {key} ===> {value} {(defValue.Equals(value) ? "(DEFAULT)" : "")}");
+            return value;
+        }
+
+        public ISharedPreferences setValue<T>(string key, T value = default)
+        {
+            Log.LogImplement.i("FileSharedPreferences", $"Set {key} = {value}");
+            store[key] = value;
+            File.WriteAllText(FILE_PATH, JsonConvert.SerializeObject(store, Formatting.Indented));
+            return this;
+        }
+    }
+}
---
+++ EHentaiAPI.TestConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using EHentaiAPI.Client;
+using EHentaiAPI.Client.Data;
+using EHentaiAPI.Client.Parser;
+using EHentaiAPI.ExtendFunction;
+using EHentaiAPI.UnitTest;
+using EHentaiAPI.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EHentaiAPI.TestConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new EhClient();
+            client.Cookies.Add(new System.Net.Cookie("sl", "dm_1","/", "e-hentai.org"));
+
+            client.Settings = new Settings()
+            {
+                SharedPreferences = new FileSharedPreferences()
+            };
+            client.Settings.putGallerySite(EhUrl.SITE_E);
+
+            var request = new EhRequest()
+                .setMethod(EhClient.Method.METHOD_SIGN_IN)
+                .setArgs("DarkProjector", TestSettings.Password);
+            var userName = await client.execute<string>(request);
+
+            request = new EhRequest()
+                .setMethod(EhClient.Method.METHOD_GET_GALLERY_DETAIL)
+                .setArgs("https://e-hentai.org/g/2062095/080e7efd64/");
+
+            var galleryList = await client.execute<GalleryDetail>(request);
+            
+            Console.ReadLine();
+        }
+    }
+}
