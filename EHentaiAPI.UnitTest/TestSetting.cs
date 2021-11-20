338484818f553cdde8088bcdf9888962ef28c4ae
MikiraSora <mikirasora0409@126.com>
2021-11-20 14:49:07 +0800

    checkout TestSetting

---
+++ EHentaiAPI.UnitTest/TestSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHentaiAPI.UnitTest
+{
+    public static partial class TestSettings
+    {
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+    }
+}
